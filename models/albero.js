99663b55cb469b5e736a3371942aabfa19263b39
SimonKomjanc <48689728+SimonKomjanc@users.noreply.github.com>
2019-04-04 11:52:12 +0200

    Add files via upload

--- models/albero.js
+++ models/albero.js
@@ -1,12 +1,11 @@
 function creaAlbero() {
 
+  
+  var troncoGeometry = new THREE.BoxGeometry(1,9,1);
   var textureTrunkAlbero = THREE.ImageUtils.loadTexture('textures/trunk_texture.jpg');
-
-  var troncoGeometry = new THREE.BoxGeometry(2,0,1);
-  var troncoMaterial = new THREE.MeshBasicMaterial({map: textureTrunkAlbero, side: THREE.DoubleSide});
-
+  var troncoMaterial = new THREE.MeshBasicMaterial({map: textureTrunkAlbero});
   var tronco = new THREE.Mesh(troncoGeometry,troncoMaterial);
-  tronco.position.set(-1,0,3);
+  //tronco.position.set(-1,0,3);
 
   return tronco;
 }
