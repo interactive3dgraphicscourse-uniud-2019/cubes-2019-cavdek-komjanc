479370c92580d785919d615ec0166b0c63ec7101
Cavdek <acavdek@gmail.com>
2019-04-04 11:35:35 +0200

    Aggiunta albero.js e la texture

---
+++ models/albero.js
@@ -0,0 +1,10 @@
+function creaAlbero() {
+
+  var troncoGeometry = new THREE.BoxGeometry(2,0,1);
+  var troncoMaterial = new THREE.MeshBasicMaterial({color:colore, side: THREE.DoubleSide});
+
+  var tronco = new THREE.Mesh(troncoGeometry,troncoMaterial);
+  tronco.position.set(-1.0.3);
+
+  return tronco;
+}
