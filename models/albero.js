adaf35d0f0602d8a8a2a16a3a63c3a50230bc506
Cavdek <acavdek@gmail.com>
2019-04-04 12:41:21 +0200

    Aggiunto un albero con texture

--- models/albero.js
+++ models/albero.js
@@ -1,11 +1,19 @@
 function creaAlbero() {
 
-  
   var troncoGeometry = new THREE.BoxGeometry(1,9,1);
   var textureTrunkAlbero = THREE.ImageUtils.loadTexture('textures/trunk_texture.jpg');
   var troncoMaterial = new THREE.MeshBasicMaterial({map: textureTrunkAlbero});
   var tronco = new THREE.Mesh(troncoGeometry,troncoMaterial);
   //tronco.position.set(-1,0,3);
+
+  var foglieGeometry = new THREE.BoxGeometry(5,2,5);
+  var textureFoglie = new THREE.ImageUtils.loadTexture('textures/foglie_texture.jpg');
+  var foglieMaterial = new THREE.MeshBasicMaterial({map: textureFoglie});
+  var foglie = new THREE.Mesh(foglieGeometry,foglieMaterial);
+
+  foglie.position.set(0,5,0);
+
+  tronco.add(foglie);
 
   return tronco;
 }
