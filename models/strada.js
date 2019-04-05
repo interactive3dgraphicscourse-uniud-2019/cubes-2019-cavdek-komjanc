03e6f23bf94f797dada5900925374a6d64fee9bd
SimonKomjanc <48689728+SimonKomjanc@users.noreply.github.com>
2019-04-05 18:24:48 +0200

    Add files via upload

--- models/strada.js
+++ models/strada.js
@@ -1,6 +1,6 @@
 function creaStrada() {
 
-  var stradaGeometry = new THREE.BoxGeometry(100,0.2,18);
+  var stradaGeometry = new THREE.BoxGeometry(130,0.2,18);
   var textureStrada = new THREE.ImageUtils.loadTexture('textures/road.jpg');
   var stradaMaterial = new THREE.MeshBasicMaterial({map: textureStrada});
   var strada = new THREE.Mesh(stradaGeometry,stradaMaterial);
